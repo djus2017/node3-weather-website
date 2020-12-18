ee03b534e6572538c85c92c266933b342c42cf87
DJUS2017 <digvijaypalsingh74@gmail.com>
2020-12-18 09:46:09 -0600

    adding % after the humidity value

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions + ', It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Current humidity is ' + body.current.humidity + ' and the wind speed is ' + body.current.wind_speed + '.')
+            callback(undefined, body.current.weather_descriptions + ', It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Current humidity is ' + body.current.humidity + '% and the wind speed is ' + body.current.wind_speed + '.')
         }
     })
 }
